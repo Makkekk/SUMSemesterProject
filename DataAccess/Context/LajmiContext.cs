0685b9c13d78bb6b47585efac2113af3c2930052
Karsten <karsten-kirkegaard@hotmail.com>
2026-04-14 11:01:08 +0200

    Working project without mongodb

--- DataAccess/Context/LajmiContext.cs
+++ DataAccess/Context/LajmiContext.cs
@@ -1,17 +1,7 @@
-using MongoDB.Driver;
 using Models;
 
 namespace DataAcces.Context;
 
-public class LajmiContext
-{
-    private readonly IMongoDatabase _database;
-
-    public LajmiContext(IMongoClient client, string databaseName)
-    {
-        _database = client.GetDatabase(databaseName);
-    }
-
-    public IMongoCollection<Company> Companies =>
-        _database.GetCollection<Company>("companies");
+public class LajmiContext {
+    
 }
--- Models/Model/Company.cs
+++ Models/Model/Company.cs
@@ -1,12 +1,8 @@
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 
 namespace Models;
 
 public class Company
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
     public string Name { get; set; } = string.Empty;
