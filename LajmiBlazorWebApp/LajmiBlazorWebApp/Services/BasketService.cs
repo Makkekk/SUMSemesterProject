8016767c2d79e74bfd83a68ac6d6f794d3470023
jamal christensen alaoui <jamalchristensen@gmail.com>
2026-04-15 16:51:37 +0200

    User story er implementeret

--- LajmiBlazorWebApp/LajmiBlazorWebApp/Services/BasketService.cs
+++ LajmiBlazorWebApp/LajmiBlazorWebApp/Services/BasketService.cs
@@ -1,12 +1,15 @@
+using Models;
+
 namespace LajmiBlazorWebApp.Services;
 
 public class BasketService
 {
     
+    public List<Product> Items { get; private set; } = new();    
     // Her sikre vi at kun metoderne i denne klasse kan ændre NumOfProducts. Indkapsling
     private int _NumOfProducts = 0;
     
-    public int NumOfProducts => _NumOfProducts;
+    public int NumOfProducts => _NumOfProducts; // --> Vi sender altid en reference ud og ikke det "rigtige" num
 
     public event Action? OnChange;
 
@@ -16,8 +19,28 @@ public class BasketService
         _NumOfProducts++;
         NotifyStateChanged();
     }
+
+    public void AddProduct(Product product)
+    {
+        Items.Add(product);
+        NotifyStateChanged();
+    }
     
     
     public void NotifyStateChanged() => OnChange?.Invoke();
+    
+    
+    public decimal GetToalPrice()
+    {
+        decimal totalPrice = 0;
+
+        foreach (var product in Items)
+        {
+            totalPrice += product.Price;
+        }
+        
+        return totalPrice;
+    }
 
 }
+
