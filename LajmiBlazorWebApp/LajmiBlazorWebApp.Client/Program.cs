3bdbc4b40e10993c8a55c6a2f5768d6b4ccbde7a
jamal christensen alaoui <jamalchristensen@gmail.com>
2026-04-16 11:04:23 +0200

    lagt pages som skal renderes ud til client og rette rendermode for hele projektet

--- LajmiBlazorWebApp/LajmiBlazorWebApp.Client/Program.cs
+++ LajmiBlazorWebApp/LajmiBlazorWebApp.Client/Program.cs
@@ -1,3 +1,4 @@
+using LajmiBlazorWebApp.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace LajmiBlazorWebApp.Client;
@@ -7,6 +8,9 @@ class Program
     static async Task Main(string[] args)
     {
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        
+        
+        builder.Services.AddScoped<BasketService>();
 
         await builder.Build().RunAsync();
     }
