8cea0a634b78555f6d5aa5018e729d203e19bc91
dotdotcommaline <nikolaj9183@gmail.com>
2026-04-16 10:24:01 +0200

    tilføjet kunde table i context

--- DataAccess/Context/LajmiContext.cs
+++ DataAccess/Context/LajmiContext.cs
@@ -4,6 +4,8 @@ using Models;
 namespace DataAcces.Context;
 
 public class LajmiContext : DbContext {
+    public DbSet<Customer> Customer { get; set; }
+    
     public LajmiContext(DbContextOptions<LajmiContext> options) : base(options) {
         
     }
