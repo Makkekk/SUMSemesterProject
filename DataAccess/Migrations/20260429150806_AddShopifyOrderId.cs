24de6eca2377f6cf96e2427c94c0d2d5f5f9a146
TueDarlingPedersen <tue.pedersen4@gmail.com>
2026-04-29 19:31:22 +0200

    WIP: Shopify integration og model justering

---
+++ DataAccess/Migrations/20260429150806_AddShopifyOrderId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAcces.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShopifyOrderId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ShopifyOrderId",
+                table: "Order",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShopifyOrderId",
+                table: "Order");
+        }
+    }
+}
